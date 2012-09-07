c10ee6751029b9a61775f02f8465c4dcfa9a7a20
Gabriel Horner and Daniel Higginbotham <pair+dh+gh@thinkrelevance.com>
2012-09-07 15:05:11 -0400

    Initial set of client methods

--- client.rb
+++ client.rb
@@ -1,12 +1,35 @@
-require 'httparty'
 require 'pp'
 require 'rest-client'
-class Client
-  include HTTParty
 
-  base_uri 'localhost:9000'
+module Datomic
+  class Client
+    def initialize(url, storage)
+      @url = url
+      @storage = storage
+    end
 
+    def create_database(dbname)
+      RestClient.put db(dbname), {}
+    end
+
+    def database_info(dbname)
+      RestClient.get db(dbname)
+    end
+
+    def datoms(dbname, index)
+      raise ArgumentError if !%w{eavt aevt avet vaet}.include?(index)
+      RestClient.get "#{db(dbname)}/datoms/#{index}"
+    end
+
+    private
+
+    def db(dbname)
+      full_storage_url + "/#{dbname}"
+    end
+
+    def full_storage_url
+      "#{@url}/db/#{@storage}"
+    end
+  end
 
 end
-
-pp RestClient.put('http://localhost:9000/db/socrates/daniel', {})
