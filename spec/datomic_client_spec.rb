967b19abdbf22607f16d35529d2041c76cf2bb5d
Gabriel Horner <gabriel.horner@gmail.com>
2012-09-14 09:55:47 -0400

    tweak tests now that query and transact work
    also only use bare minimum schema

--- spec/datomic_client_spec.rb
+++ spec/datomic_client_spec.rb
@@ -54,13 +54,14 @@ describe Datomic::Client do
       resp = client.transact('test-transact', schema)
       resp.code.should == 200
       resp.body.should be_a(Hash)
+      resp.body.keys.sort.should == [:"db-after", :"db-before", :tempids, :"tx-data"]
     end
 
     it "returns correct response with array of data" do
-      # TODO these are magic spooky values
-      resp = client.transact('test-transact', [[:"db/add", 1, 61, "value"]])
+      resp = client.transact('test-transact', [[:"db/add", 1, :"community/name", "Some Community"]])
       resp.code.should == 200
       resp.body.should be_a(Hash)
+      resp.body.keys.sort.should == [:"db-after", :"db-before", :tempids, :"tx-data"]
     end
   end
 
@@ -123,12 +124,21 @@ describe Datomic::Client do
     before {
       client.create_database('test-query')
       client.transact('test-query', schema)
+      client.transact('test-query', [[:"db/add", 1, :"community/name", "Some Community"]])
     }
 
     it "returns a correct response with a string query" do
       resp = client.query('test-query', '[:find ?c :where [?c :community/name]]')
       resp.code.should == 200
       resp.body.should be_a(Array)
+      resp.body.should == [[1]]
+    end
+
+    it "returns a correct response with limit param" do
+      resp = client.query('test-query', '[:find ?c :where [?c :community/name]]', :limit => 0)
+      resp.code.should == 200
+      resp.body.should be_a(Array)
+      resp.body.should == []
     end
 
     it "returns a correct response with a data query" do
